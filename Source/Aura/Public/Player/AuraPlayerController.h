9f5097de0d964aa3aed968358a2423adbe1cb69f
WSzewczykSpD <wszewczyk.spd@gmail.com>
2024-09-05 23:07:31 +0200

    Player Controller and UE upgrade

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -3,9 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
+//#include "../../../../../../UE_5.1/Engine/Plugins/EnhancedInput/Source/EnhancedInput/Public/InputMappingContext.h"
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+class UInputMappingContext; 
 /**
  * 
  */
@@ -13,5 +15,15 @@ UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	AAuraPlayerController();
+
 	
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
 };
